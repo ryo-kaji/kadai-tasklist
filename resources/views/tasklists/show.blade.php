7377958d9e69014d19cde46e9f93759952aa6164
ryo-kaji <ryo0329liang@gmail.com>
2018-06-05 06:04:10 +0000

    bootstrap design

--- resources/views/tasklists/show.blade.php
+++ resources/views/tasklists/show.blade.php
@@ -4,13 +4,25 @@
 
     <h1>id = {{ $tasklist->id }} のタスク詳細ページ</h1>
 
-    <p>ステータス: {{ $tasklist->status }}</p>
-    <p>タスク: {{ $tasklist->content }}</p>
+     <table class="table table-bordered">
+        <tr>
+            <th>id</th>
+            <td>{{ $tasklist->id }}</td>
+        </tr>
+        <tr>
+            <th>ステータス</th>
+            <td>{{ $tasklist->title }}</td>
+        </tr>
+        <tr>
+            <th>タスク</th>
+            <td>{{ $tasklist->content }}</td>
+        </tr>
+    </table>
 
-    {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id]) !!}
+    {!! link_to_route('tasklists.edit', 'このタスクを編集', ['id' => $tasklist->id], ['class' => 'btn btn-default']) !!}
     
     {!! Form::model($tasklist, ['route' => ['tasklists.destroy', $tasklist->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
     
 @endsection
